3126846a39c637f6e244561a9acfee46aced545a
ender <xuender@gmail.com>
2018-08-01 16:52:17 +0800

    fix #9

--- main.go
+++ main.go
@@ -3,24 +3,36 @@ package main
 import (
 	"log"
 	"os"
+	"strings"
 
-	"./hd"
 	"github.com/urfave/cli"
 	"github.com/xuender/goutils"
+
+	"./hd"
 )
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "home-disk"
 	app.Usage = "家庭数据盘"
+	app.Version = "0.0.1"
 	app.Flags = []cli.Flag{
 		cli.BoolFlag{
 			Name:  "browser,b",
 			Usage: "启动禁止打开浏览器",
 		},
+		cli.StringFlag{
+			Name:  "port,p",
+			Value: "6181",
+			Usage: "访问端口",
+		},
 	}
 	app.Action = func(c *cli.Context) error {
-		var port = ":6181"
+		var port = c.String("port")
+		if !strings.HasPrefix(port, ":") {
+			port = ":" + port
+		}
+
 		// 打开浏览器
 		if !c.Bool("browser") {
 			url, err := hd.GetUrl(port)
