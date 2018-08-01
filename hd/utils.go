266ef1b94e6b6387df99fef4b3596c2a1ef35be8
ender <xuender@gmail.com>
2018-08-01 16:29:44 +0800

    update #4

---
+++ hd/utils.go
@@ -0,0 +1,39 @@
+package hd
+import (
+	"fmt"
+	"strings"
+	"net"
+	"errors"
+	"os"
+)
+// 获取网址
+func GetUrl(port string) (string, error) {
+	ip, err := GetIp()
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("http://%s%s", ip, port), nil
+}
+
+// 获取IP地址
+func GetIp() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil && !strings.HasPrefix(ipnet.IP.String(), "172") {
+				return ipnet.IP.String(), nil
+			}
+		}
+	}
+	return "", errors.New("未找到IP")
+}
+
+func mkdir(path string){
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		os.MkdirAll(path, 0777)
+	}
+}
---
+++ hd/web.go
@@ -0,0 +1,93 @@
+package hd
+import (
+	"os"
+	"io"
+	"rsc.io/qr"
+	"net/http"
+	"log"
+	"github.com/labstack/echo"
+	"github.com/xuender/goutils"
+	"github.com/labstack/echo/middleware"
+)
+type Web struct {
+	Port string    // 端口号
+	Temp string // 临时文件目录
+	Data string // 保存数据目录
+}
+
+func (w *Web) Run(){
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{echo.POST},
+		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowCredentials: true,
+	}))
+
+	e.Static("/", "www")
+	// Routes
+	e.GET("/qr", w.qrcode)
+	e.POST("/up", w.upload)
+
+	// Start server
+	e.Logger.Fatal(e.Start(w.Port))
+}
+func (w *Web)upload(c echo.Context) error {
+	c.Logger().Warn("文件上传")
+	file, err := c.FormFile("file")
+	if err != nil {
+		return err
+	}
+	src, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	mkdir(w.Data)
+	mkdir(w.Temp)
+	// 目的
+	f:=w.Temp + string(os.PathSeparator) + file.Filename
+	dst, err := os.Create(f)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	// 复制
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+	// TODO 保存FileId
+	fid, err:=goutils.NewFileId(f)
+	if err !=nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+	log.Println(fid)
+	r, err:=NewResource(f)
+	if err !=nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+	mkdir(r.Path(w.Data))
+	os.Rename(f, r.FullName(w.Data))
+	return c.String(http.StatusOK, "File is uploaded")
+}
+
+// QR码
+func (w *Web)qrcode(c echo.Context) error {
+	url, err := GetUrl(w.Port)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+	code, err := qr.Encode(url, qr.Q)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "QR码生成错误")
+	}
+	return c.Blob(http.StatusOK, "image/png", code.PNG())
+}
+
